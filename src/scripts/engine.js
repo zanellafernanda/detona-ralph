37a2b4a6e288de74f6d230ad8a0e3a91af3b7eae
Fernanda Zanella <zanella.fernanda@gmail.com>
2024-01-21 21:22:47 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,73 @@
+
+const state = {
+
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    //limpar a classe de inimigos
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    //sortear número aleatório
+    let randomNumber = Math.floor(Math.random() * 9);
+    //selecionar o quadrado correspondente ao número aleatório
+    let randomSquare = state.view.squares[randomNumber];
+    //adicionar a classe inimigo neste quadrado
+    randomSquare.classList.add("enemy");
+    //guardar o id do quadrado aleatório que eu sorteei
+    state.values.hitPosition = randomSquare.id;
+
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+}
+
+function initialize() {
+    addListenerHitBox();
+}
+
+initialize();
